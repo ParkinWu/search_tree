a67bb46d922aa8790f8f614f9d256af9e9368386
parkinwu <snowy_way@163.com>
2016-08-16 13:28:52 +0800

    add search

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,33 @@ struct SearchTree<T> {
     key: T,
 }
 
-impl<T> SearchTree<T> where T: std::fmt::Debug {
+impl<T> SearchTree<T> where T: std::fmt::Debug + std::cmp::Eq + std::cmp::PartialOrd {
     fn inorder_tree(self) {
         self.left.map(|x| x.inorder_tree());
         println!("self.key = {:#?}", self.key);
         self.right.map(|x| x.inorder_tree());
     }
+
+    fn search_tree(self, key: T ) -> Option<SearchTree<T>> {
+        if self.key == key {
+            return Some(self);
+        }
+
+        if key < self.key {
+            match self.left {
+                Some(tree) => tree.search_tree(key),
+                None => None
+            }
+        } else {
+            match self.right {
+                Some(tree) => tree.search_tree(key),
+                None => None
+            }
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
     let left = SearchTree {
         left: None,
         right: None,
@@ -34,6 +51,14 @@ fn main() {
         right: Some(Box::new(right)),
         key: 40,
     };
-    root.inorder_tree();
+
+//    root.inorder_tree();
+
+    let tree = root.search_tree(43);
+    match tree {
+        Some(t) => println!("t.key = {:#?}", t.key),
+        None => println!(" None "),
+
+    }
 
 }
