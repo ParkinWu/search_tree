d98d5c8643f2e84de72db412bd898c2d546701e1
parkinwu <snowy_way@163.com>
2016-08-19 13:54:39 +0800

    实验

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 extern crate time;
 
 
-
 #[derive(Debug)]
 struct SearchTree<T> {
     left: Option<Box<SearchTree<T>>>,
@@ -57,32 +56,54 @@ impl<T> SearchTree<T> where T: std::fmt::Debug + std::cmp::Eq + std::cmp::Partia
             None => self.key.clone()
         }
     }
+//    fn insert(&mut self, val: T) {
+//        let mut node;
+//        if val <= self.key {
+//            node = self.left;
+//        } else {
+//            node = self.right;
+//        }
+//
+//        loop {
+//            match node {
+//                Some(n) => {
+//                    if val <= n.key {
+//                        node = n.left;
+//                    } else {
+//                        node = n.right;
+//                    }
+//                },
+//                None => break,
+//            }
+//        }
+//
+//    }
     /// 插入一个节点
-    fn insert(&mut self, val: T) {
-        if self.key >= val {
-            if let Some(ref mut left) = self.left {
-                left.insert(val);
-            } else {
-                self.left = Some(Box::new(SearchTree {
-                    left: None,
-                    right: None,
-                    key: val,
-
-                }));
-            }
-        } else {
-            if let Some(ref mut right) = self.right {
-                right.insert(val);
-            } else {
-                self.right = Some(Box::new(SearchTree {
-                    left: None,
-                    right: None,
-                    key: val,
-
-                }));
-            }
-        }
-    }
+//    fn insert(&mut self, val: T) {
+//        if self.key >= val {
+//            if let Some(ref mut left) = self.left {
+//                left.insert(val);
+//            } else {
+//                self.left = Some(Box::new(SearchTree {
+//                    left: None,
+//                    right: None,
+//                    key: val,
+//
+//                }));
+//            }
+//        } else {
+//            if let Some(ref mut right) = self.right {
+//                right.insert(val);
+//            } else {
+//                self.right = Some(Box::new(SearchTree {
+//                    left: None,
+//                    right: None,
+//                    key: val,
+//
+//                }));
+//            }
+//        }
+//    }
 
 
     /// 删除一个节点
@@ -91,36 +112,94 @@ impl<T> SearchTree<T> where T: std::fmt::Debug + std::cmp::Eq + std::cmp::Partia
     }
 
 }
+use std::mem::size_of;
+
+//fn main() {
+
+//    let mut v = Vec::new();
+//    for i in 0..330000 {
+//        v.push(SearchTree {
+//            left: None,
+//            right: None,
+//            key: 100,
+//        });
+//    }
+
+
+
+//
+//    let mut root = SearchTree {
+//        left: None,
+//        right: None,
+//        key: 330001,
+//    };
+////    let mut hash_map = HashMap::new();
+//    for i in 0..330000 {
+//        root.insert(i);
+//    }
+//    println!("insert end");
+//    let mut start = timestamp();
+//    let result = hash_map.keys().filter(|&&x| (x >= 1000000 - 1)).collect::<Vec<_>>();
+//    let mut end = timestamp();
+//    println!("end - start = {:#?}", end - start);
+//
+//    start = timestamp();
+//    let max = root.maximum();
+//    end = timestamp();
+//    println!("end - start = {:#?}", end - start);
+//    println!("max = {:#?}", max);
+//}
+
+
+//fn timestamp() -> f64 {
+//    let timespec = time::get_time();
+//    // 1459440009.113178
+//    let mills: f64 = timespec.sec as f64 + (timespec.nsec as f64 / 1000.0 / 1000.0 / 1000.0 );
+//    mills
+//
+//}
+//
+//use std::ptr;
+//
+//struct RawLink<T> {
+//    p: *mut T,
+//}
+//
+//fn none<T>() -> RawLink<T> {
+//    RawLink { p: ptr::null_mut() }
+//}
+//
+//fn some<T>(n: &mut T) -> RawLink<T> {
+//    RawLink { p: n as *mut T }
+//}
+//
+//struct Node<T> {
+//    next: Option<Box<Node<T>>>,
+//    prev: Option<Box<Node<T>>>,
+//    val:  T,
+//}
+//
+//struct List<T> {
+//    list_head: Option<Node<T>>,
+//    list_tail: Option<Node<T>>,
+//}
+//
+//impl<T> List<T> {
+//    fn push_front_node(&mut self, mut new_head: Node<T>) {
+//        match self.list_head {
+//            None => {
+//                self.list_tail = Some(Box::new(new_head));
+//                new_head.prev = None;
+//                self.list_head = Some(Box::new(new_head));
+//            },
+//            Some(ref mut head) => {
+//
+//            }
+//        }
+//    }
+//}
 
 fn main() {
-    let mut root = SearchTree {
-        left: None,
-        right: None,
-        key: 10000001,
-    };
-    let mut hash_map = HashMap::new();
-    for i in 1000..100000000 {
-        hash_map.insert(i, i);
-//        root.insert(i);
-    }
-    println!("insert end");
-    let mut start = timestamp();
-    let result = hash_map.keys().filter(|&&x| (x >= 1000000 - 1)).collect::<Vec<_>>();
-    let mut end = timestamp();
-    println!("end - start = {:#?}", end - start);
-
-    start = timestamp();
-    let max = root.maximum();
-    end = timestamp();
-    println!("end - start = {:#?}", end - start);
-    println!("max = {:#?}", max);
-}
-
-fn timestamp() -> f64 {
-    let timespec = time::get_time();
-    // 1459440009.113178
-    let mills: f64 = timespec.sec as f64 + (timespec.nsec as f64 / 1000.0 / 1000.0 / 1000.0 );
-    mills
 
 }
 
