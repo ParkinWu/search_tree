1be69d02cb888ee9e3e1d725554566f4a88a88b2
parkinwu <snowy_way@163.com>
2016-08-26 14:28:17 +0800

    execise

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,4 @@
 use std::ptr::null_mut;
-struct Node<T> {
-    val: T,
-    next: *mut Box<Node<T>>
-}
-
-struct LinkedList<T> {
-    head: *mut Box<Node<T>>,
-    tail: *mut Box<Node<T>>,
-}
-
-impl<T> LinkedList<T> {
-    fn new() -> LinkedList<T> {
-        return LinkedList {
-            head: null_mut(),
-            tail: null_mut(),
-        };
-
-    }
-//    fn append(&self, val: T) {
-//        let
-//    }
-}
 
 #[derive(Debug)]
 struct Person {
